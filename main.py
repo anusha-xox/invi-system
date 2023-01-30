7cc6ce1de8a414f3d926ca5615811438eed23852
anusha-xox <anushajain.bang@gmail.com>
2023-01-30 05:43:30 +0530

    admin functionality added

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL, Email
 from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import email_validator
+from flask_bootstrap import Bootstrap
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_BINDS'] = {'faculty': 'sqlite:///faculty.db',
-                                  'subject': 'sqlite:///subject.db'}
+                                  'subject': 'sqlite:///subject.db'
+                                  'admin': 'sqlite:///admin.db'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -50,6 +54,13 @@ class Subject(db.Model):
     __bind_key__ = 'subject'
     id = db.Column(db.Integer, primary_key=True)
 
+class Admin(db.Model):
+    __bind_key__ = 'admin'
+    fac_id = db.Column(db.String(250), primary_key=True)
+    group_id = db.Column(db.String(250), unique=False, nullable=False)
+
+#db.create_all()
+
 
 class FacultyForm(FlaskForm):
     fac_id = StringField('Faculty Id', validators=[DataRequired()])
@@ -61,11 +72,11 @@ class FacultyForm(FlaskForm):
     phone_no = StringField('Phone Number', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class AdminForm(FlaskForm):
+    fac_id = StringField('Faculty Id', validators=[DataRequired()])
+    group_id = StringField('Group Id', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-# db.create_all()
-
-# Line below only required once, when creating DB.
-# db.create_all()
 
 @app.route('/')
 def home():
@@ -93,12 +104,15 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
-        requested_post = User.query.get(email)
-        if check_password_hash(user.password, password):
-            login_user(user)
-            # return redirect(url_for('secrets', display_name=user.name))
-            return redirect(url_for('add_faculty', display_name=user.name))
+        if email == 'admin@email.com' and password == 'admin':
+            return redirect(url_for('admin'))
+        else:
+            user = User.query.filter_by(email=email).first()
+            requested_post = User.query.get(email)
+            if check_password_hash(user.password, password):
+                login_user(user)
+                # return redirect(url_for('secrets', display_name=user.name))
+                return redirect(url_for('add_faculty', display_name=user.name))
     return render_template("login.html")
 
 
@@ -108,15 +122,36 @@ def secrets():
     display_name = request.args.get("display_name")
     return render_template("secrets.html", display_name=display_name)
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    form = AdminForm()
+    if form.validate_on_submit():
+        new_allotment = Admin(
+            fac_id=form.fac_id.data,
+            group_id=form.group_id.data,
+        )
+        db.session.add(new_allotment)
+        db.session.commit()
+    return render_template("admin.html", form=form)
 
 @app.route('/add-faculty', methods=["GET", "POST"])
 def add_faculty():
     form = FacultyForm()
     display_name = request.args.get("display_name")
     if form.validate_on_submit():
-        pass
+        new_faculty = Faculty(
+            fac_id=form.fac_id.data,
+            fac_email=form.fac_email.data,
+            fac_fname=form.fac_fname.data,
+            fac_mname=form.fac_mname.data,
+            fac_lname=form.fac_lname.data,
+            phone_no=form.phone_no.data,
+            group_id=0,
+            dept_no=0,
+        )
+        db.session.add(new_faculty)
+        db.session.commit()
     return render_template("add_faculty.html", form=form, display_name=display_name)
-
 
 @app.route('/logout')
 def logout():
